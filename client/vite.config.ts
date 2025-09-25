4de74de49645e2156267583330e9859635fb6b0a
Byrix <Byrix99@pm.me>
2025-09-25 11:16:47 +1000

    config alter

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,27 +1,44 @@
-import type { UserConfig } from 'vite';
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import type { UserConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-import preact from '@preact/preset-vite'
-import tailwindcss from '@tailwindcss/vite';
-import alias from '@rollup/plugin-alias';
-import path from 'path';
+const cesiumSource = "../node_modules/cesium/Build/Cesium";
+const cesiumBaseUrl = "cesiumStatic";
 
 // https://vite.dev/config/
 export default {
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '$': path.resolve(__dirname, './src'),
-    }
-  },
-  plugins: [
-    preact(), 
-    tailwindcss(),
-  ],
+	server: {
+		proxy: {
+			"/openapi": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+			},
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			$: path.resolve(__dirname, "./src"),
+		},
+	},
+	plugins: [
+		preact(),
+		tailwindcss(),
+		viteStaticCopy({
+			targets: [
+				{ src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+				{ src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+				{ src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+				{ src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+			],
+		}),
+	],
+	define: {
+		CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
+	},
 } satisfies UserConfig;
