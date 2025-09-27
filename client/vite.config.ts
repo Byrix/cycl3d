b0b6d3c13969fbfa91f57d6d653d446ef0ecd52d
Byrix <Byrix99@pm.me>
2025-09-27 18:04:28 +1000

    config updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,44 +1,48 @@
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import type { UserConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const cesiumSource = "../node_modules/cesium/Build/Cesium";
 const cesiumBaseUrl = "cesiumStatic";
 
-// https://vite.dev/config/
-export default {
-  server: {
-    proxy: {
-      "/openapi": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        openapi: {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+        },
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+        },
+        "/wms": {
+          target: env.GEOSERVER_BASE,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      $: path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: { "$": path.resolve(__dirname, "./src") },
     },
-  },
-  plugins: [
-    preact(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
-      ],
-    }),
-  ],
-  define: {
-    CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
-  },
-} satisfies UserConfig;
+    plugins: [
+      preact(),
+      tailwindcss(),
+      viteStaticCopy({
+        targets: [
+          { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+        ],
+      }),
+    ],
+    define: {
+      CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
+    },
+  };
+});
